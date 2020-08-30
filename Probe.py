f8255207da3533961ced2f872d2cafd016f7e60e
gbrs <gbrs@mail.ru>
2020-08-30 21:40:10 +0300

    Добавил Льюиса Кэрролла файлик

--- Probe.py
+++ Probe.py
@@ -1,3 +1,25 @@
-a = 9/4
-b = 13.5/6
-print(a is b)
+# number = '8' * 88
+# print(number)
+
+
+# txt = ''
+# with open('caroll.txt', encoding='utf-8') as f:
+#     for line in f:
+#         txt += line.strip() + ' '
+# for i in ('.', ',', ';', ':', '—', '?', '!', "'", '-'):
+#     txt = txt.replace(i, '')
+# print(txt[:100].lower())
+# print(list(txt[:100].lower().split()))
+
+def F(n):
+    if n > 2:
+        return F(n-1) + G(n-2)
+    else:
+        return 2
+def G(n):
+    if n > 2:
+        return G(n-1) + F(n-2)
+    else:
+        return 2
+
+print(F(20))
